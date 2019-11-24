0efb50167ef3a57aae3c5ebf29e03d05bda3cd40
Guayasén Suárez <gsuarezg@gmail.com>
2019-11-24 19:43:46 +0000

    QuizApp: Login page design

---
+++ front/src/app/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent {
+    public username = null;
+    public password = null;
+
+}
